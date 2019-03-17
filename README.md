e6f053efa88205a341b876c8a42c6d5e3f1b0c3b
Tomasz Gemza <t.gemza@secit.pl>
2019-03-17 15:40:25 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# text-to-color
-Make a RGB color from a given string
+# Simple text to RGB color converter
+
+This library allows to create RGB color from a given string.
+The usage is as simple as possible (one line). The returned value
+for each string will be the same for each execution so it's ideal
+for generating dedicated user color from his/her name/username/email etc.
+
+## Installation
+
+From the command line run
+
+```
+$ composer require secit-pl/text-to-color
+```
+
+## Usage
+
+```php
+<?php
+
+use SecIT\TextToColor;
+
+echo TextToColor::toRGB('some text');
+```
+
+## Example outputs
+
+| Input        | Output  |
+|--------------|---------|
+| example foo  | #17E500 |
+| example      | #E50066 |
+| foo bar      | #0059E5 |
+| test         | #7800E5 |
+| example text | #D300E5 |
+| Username     | #6DE500 |
---
+++ TextToColor.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SecIT;
+
+/**
+ * Class TextToColor.
+ *
+ * Text to color converter inspired on this stack overflow post https://stackoverflow.com/a/3724219/10561527
+ *
+ * @author Tomasz Gemza
+ */
+class TextToColor
+{
+    /**
+     * Create RGB string (eg. #123456) from a given text.
+     *
+     * @param string $text
+     * @param float  $saturation
+     * @param float  $lightness
+     *
+     * @return string
+     */
+    public static function toRGB(string $text, float $saturation = 1, float $lightness = 0.9): string
+    {
+        $hue = unpack('L', hash('adler32', $text, true))[1];
+
+        return self::hsl2rgb($hue/0xFFFFFFFF, $saturation, $lightness);
+    }
+
+    /**
+     * HSL to RGB converter.
+     *
+     * @param $hue
+     * @param $saturation
+     * @param $lightness
+     *
+     * @return string
+     */
+    private static function hsl2rgb(float $hue, float $saturation, float $lightness): string
+    {
+        $hue *= 6;
+        $h = intval($hue);
+        $hue -= $h;
+        $lightness *= 255;
+        $m = $lightness * (1 - $saturation);
+        $x = $lightness * (1 - $saturation * (1 - $hue));
+        $y = $lightness * (1 - $saturation * $hue);
+        $a = [
+            [$lightness, $x, $m],
+            [$y, $lightness, $m],
+            [$m, $lightness, $x],
+            [$m, $y, $lightness],
+            [$x, $m, $lightness],
+            [$lightness, $m, $y]
+        ][$h];
+
+        return sprintf("#%02X%02X%02X", $a[0], $a[1], $a[2]);
+    }
+}
+
+ini_set('display_errors', 'on');
+//ini_set('error_reporting', E_ALL);
+
+
+
+printf('<span style="color: %s">%s</span>', TextToColor::toRGB($_GET['x']), $_GET['x']);
+
+?>
+
+<pre>
+    <?php
+
+    foreach(['example foo', 'example', 'foo bar', 'test','example text', 'Username'] as $text) {
+        echo $text.' - '.TextToColor::toRGB($text)."     ";
+        printf('<span style="color: %s">%s</span>', TextToColor::toRGB($text), $text);
+        echo "\n";
+    }
+
+    ?>
+</pre>
